f075565358ee78a73f13eecb6d272a7071f1581a
Amy Edwardson <am4zed11@gmail.com>
2019-05-31 14:50:19 +0100

    added develop branch

---
+++ foodbook-backend/foodBook-backend/src/main/java/com/FoodBook/foodBook/FoodBookApplication.java
@@ -0,0 +1,13 @@
+package com.FoodBook.foodBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBookApplication.class, args);
+	}
+
+}
