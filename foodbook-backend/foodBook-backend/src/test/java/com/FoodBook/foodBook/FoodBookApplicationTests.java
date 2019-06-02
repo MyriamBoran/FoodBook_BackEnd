f343944e9f498a1e47cd070f14553d30209acfdf
Amy Edwardson <am4zed11@gmail.com>
2019-06-02 21:43:44 +0100

    attempting to pass getFavouriteRecipesById() test

--- foodbook-backend/foodBook-backend/src/test/java/com/FoodBook/foodBook/FoodBookApplicationTests.java
+++ foodbook-backend/foodBook-backend/src/test/java/com/FoodBook/foodBook/FoodBookApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FoodBookApplicationTests {
@@ -37,5 +39,11 @@ public class FoodBookApplicationTests {
 		FavouriteRecipe goulash = new FavouriteRecipe(mary, "Goulash");
 		userRepository.save(mary);
 		favouriteRecipeRepository.save(goulash);
+	}
+
+	@Test
+	public void getFavouriteRecipesById() {
+		
+		assertEquals("Goulash", userRepository.getFavouriteRecipesById(1L));
 	}
 }
