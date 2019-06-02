36b07c0b82ad4782240b36366830f948c4e83360
Amy Edwardson <am4zed11@gmail.com>
2019-06-02 21:32:32 +0100

    updated user controller and repo

--- foodbook-backend/foodBook-backend/src/main/java/com/FoodBook/foodBook/controllers/UserController.java
+++ foodbook-backend/foodBook-backend/src/main/java/com/FoodBook/foodBook/controllers/UserController.java
@@ -1,4 +1,22 @@
 package com.FoodBook.foodBook.controllers;
 
+import com.FoodBook.foodBook.models.FavouriteRecipe;
+import com.FoodBook.foodBook.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users")
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/{id}/favourite-recipes")
+    public List<FavouriteRecipe> getFavouriteRecipesById(Long id){
+        return userRepository.getFavouriteRecipesById(id);
+    }
 }
