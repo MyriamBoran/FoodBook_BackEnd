c88b3c2a538b08ecb333fff8d9f4f94a120ebfab
Amy Edwardson <am4zed11@gmail.com>
2019-06-03 12:52:52 +0100

    Add api controller

---
+++ foodbook-backend/foodBook-backend/src/main/java/com/FoodBook/foodBook/controllers/ApiController.java
@@ -0,0 +1,13 @@
+package com.FoodBook.foodBook.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api")
+public class ApiController {
+
+    @GetMapping("/{query}")
+
+}
