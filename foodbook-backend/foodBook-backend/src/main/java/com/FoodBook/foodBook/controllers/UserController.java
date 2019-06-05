c4d7faae0c5a5174ad208af33b1512f0ee8747c1
user <gibson.stephen92@gmail.com>
2019-06-05 16:39:25 +0100

    updates user controller for cross origins

--- foodbook-backend/foodBook-backend/src/main/java/com/FoodBook/foodBook/controllers/UserController.java
+++ foodbook-backend/foodBook-backend/src/main/java/com/FoodBook/foodBook/controllers/UserController.java
@@ -5,10 +5,7 @@ import com.FoodBook.foodBook.models.FavouriteRecipe;
 import com.FoodBook.foodBook.repositories.FavouriteRecipeRepository.FavouriteRecipeRepository;
 import com.FoodBook.foodBook.repositories.UserRepository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +18,12 @@ public class UserController {
     @Autowired
     FavouriteRecipeRepository favouriteRecipeRepository;
 
+    @CrossOrigin
     @GetMapping("/{id}/favourite-recipes")
     public FavouriteRecipe getFavouriteRecipes(@PathVariable String id){
         System.out.println(id);
-        return userRepository.findFavouriteRecipes(id).get(0);
+        List<FavouriteRecipe> recipes = userRepository.findFavouriteRecipes(id);
+        if(recipes.size() == 0) return null;
+        return recipes.get(0);
     }
 }
